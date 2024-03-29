a7b460c6efd4752a47a1d9a9f7004a7b95327d4e
NSaroya <nikhilsaroya09@gmail.com>
2024-03-29 13:20:03 -0600

    Added some test data

--- src/main.cpp
+++ src/main.cpp
@@ -298,6 +298,10 @@ void loop1() {
       }
 
       if (wifiEnabled){
+          // modify the data type if needed
+          // ggaData = "$GPGGA,172814.0,3723.46587704,N,12202.26957864,W,2,6,1.2,18.893,M,-25.669,M,2.0,0031*4F"; // Test Data
+
+          // uartDataReceivedFromMaster = modify needed ... 
 
           if (!currentGPSRawData.isEmpty()){
             // Convert String to char buffer
